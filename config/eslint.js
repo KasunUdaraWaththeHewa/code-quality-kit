7e049a6f8da9c26d2b919490a8ed16583c412c0c
Kasun Udara <2021cs198@stu.ucsc.cmb.ac.lk>
2025-07-15 22:49:37 +0530

    Some prettier eslint conflict resolved

--- config/eslint.js
+++ config/eslint.js
@@ -1,9 +1,14 @@
+// config/eslint.js
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
-  env: { browser: true, node: true, es2021: true },
-  plugins: ['prettier'],
+  extends: ["eslint:recommended", "plugin:prettier/recommended"],
+  env: {
+    browser: true,
+    node: true,
+    es2021: true,
+  },
+  plugins: ["prettier"],
   rules: {
-    'prettier/prettier': 'error',
-    semi: ['error', 'always'],
+    "prettier/prettier": "error",
+    semi: ["error", "always"],
   },
 };
